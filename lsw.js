38d4a936eb1209ba3b1fc031901a7c2c1892a11c
WaiharoKanyi <bkanyitoz@gmail.com>
2023-02-16 15:27:43 +0300

    new additions to js and pop up

---
+++ lsw.js
@@ -0,0 +1,11 @@
+const signin_btn = document.querySelector("#sign_in")
+const pop_up = document.querySelector(".signin")
+
+signin_btn.addEventListener('click', () =>{
+    pop_up.style.display = 'flex'
+})
+window.addEventListener('click', (e) =>{
+    if(e.target == document.querySelector){
+
+    }
+})
